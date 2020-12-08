17a04a3c4d84069633c40d9bad6a3dffdf046d66
wepiggy-dev <coredevs@wepiggy.com>
2020-12-08 12:45:39 +0800

    update shell

--- scripts/deploy_kovan.sh
+++ scripts/deploy_kovan.sh
@@ -4,10 +4,15 @@ set -e
 
 #!!!!!!!!!!!!!------- modify here ------#
 NETWORK=kovan
-TIME_LOCK_ADMIN=
-PIGGY_BREEDER_START_BLOCK=
-PIGGY_BREEDER__ENABLE_CLAIM_BLOCK=
+TIME_LOCK_ADMIN=0x944251E03Fa3b351e01C7Fc9e1C86c6538B2D96D
+PIGGY_BREEDER_START_BLOCK=22414453
+PIGGY_BREEDER__ENABLE_CLAIM_BLOCK=22414453
 ETH=0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE
+ZERO_ADDR=0x0000000000000000000000000000000000000000
+ETH_CHAINLINK=0x9326BFA02ADD2366b30bacB125260Af641031331
+WBTC_CHAINLINK=0x6135b13325bfC4B00278B4abC5e20bbce2D6580e
+DAI_CHAINLINK=0x777A68032a88E5A84678A77Af2CD65A7b3c0775a
+
 output_file=deployed.kovan_qa.env
 
 START=`date +%s`
@@ -20,28 +25,11 @@ print_success () {
   printf "\e[4;32m$1\e[0m\n"
 }
 
-#npx oz link @openzeppelin/contracts-ethereum-package
-
 npx oz compile --solc-version=0.6.12 --optimizer on
 
 npx oz session --network $NETWORK
 
 #==============================for mock erc20s==============================#
-YFII=`npx oz deploy -k regular -n $NETWORK MockERC20 YFII.finance YFII 1e27 18`
-print_progress "YFII = $YFII"
-echo "YFII=$YFII" >> $output_file
-
-QUSD=`npx oz deploy -k regular -n $NETWORK MockERC20 QUSD\ Stablecoi QUSD 1e27 18`
-print_progress "QUSD = $QUSD"
-echo "QUSD=$QUSD" >> $output_file
-
-HUSD=`npx oz deploy -k regular -n $NETWORK MockERC20 HUSD HUSD 1e17 8`
-print_progress "HUSD = $HUSD"
-echo "HUSD=$HUSD" >> $output_file
-
-UNI=`npx oz deploy -k regular -n $NETWORK MockERC20 Uniswap UNI 1e27 18`
-print_progress "UNI = $UNI"
-echo "UNI=$UNI" >> $output_file
 
 WBTC=`npx oz deploy -k regular -n $NETWORK MockERC20 Wrapped\ BTC WBTC 1e17 8`
 print_progress "WBTC = $WBTC"
@@ -110,20 +98,9 @@ echo "PIGGY_BREEDER=$PIGGY_BREEDER" >> $output_file
 npx oz send-tx --to $WPC --method grantRole --args 0x9f2df0fed2c77648de5860a4cc508cd0818c85b8b8a1ab4ceeef8d981c8956a6,$PIGGY_BREEDER
 
 #==============================price oracle==============================#
-SIMPLE_PRICE_ORAClE=`npx oz create -n $NETWORK SimplePriceOracle --init initialize`
-print_progress "SIMPLE_PRICE_ORAClE = $SIMPLE_PRICE_ORAClE"
-echo "SIMPLE_PRICE_ORAClE=$SIMPLE_PRICE_ORAClE" >> $output_file
-
-# mock send price to SimplePriceOracle
-npx oz send-tx --to $SIMPLE_PRICE_ORAClE --method setPrice --args $ETH,598650000000000000000
-npx oz send-tx --to $SIMPLE_PRICE_ORAClE --method setPrice --args $DAI,1004481000000000000
-npx oz send-tx --to $SIMPLE_PRICE_ORAClE --method setPrice --args $USDT,1000000000000000000000000000000
-npx oz send-tx --to $SIMPLE_PRICE_ORAClE --method setPrice --args $USDC,1000000000000000000000000000000
-npx oz send-tx --to $SIMPLE_PRICE_ORAClE --method setPrice --args $WBTC,192191700000000000000000000000000
-npx oz send-tx --to $SIMPLE_PRICE_ORAClE --method setPrice --args $QUSD,1000000000000000000
-npx oz send-tx --to $SIMPLE_PRICE_ORAClE --method setPrice --args $HUSD,10000000000000000000000000000
-npx oz send-tx --to $SIMPLE_PRICE_ORAClE --method setPrice --args $YFII,19203710000000000000000
-npx oz send-tx --to $SIMPLE_PRICE_ORAClE --method setPrice --args $UNI,3931450000000000000
+WP_PRICE_PROVIDER_V1=`npx oz deploy -k regular -n $NETWORK WePiggyPriceProviderV1`
+print_progress "WP_PRICE_PROVIDER_V1 = $WP_PRICE_PROVIDER_V1"
+echo "WP_PRICE_PROVIDER_V1=$WP_PRICE_PROVIDER_V1" >> $output_file
 
 #==============================Comptroller==============================#
 COMPTROLLER=`npx oz create -n $NETWORK Comptroller --init initialize`
@@ -156,21 +133,11 @@ P_WBTC=`npx oz create -n $NETWORK PERC20 --init initialize --args $WBTC,$COMPTRO
 print_progress "P_WBTC = $P_WBTC"
 echo "P_WBTC=$P_WBTC" >> $output_file
 
-P_YFII=`npx oz create -n $NETWORK PERC20 --init initialize --args $YFII,$COMPTROLLER,$JUMP_RATE_MODEL,0.2e27,\"WePiggy\ YFII\",\"pYFII\",8`
-print_progress "P_YFII = $P_YFII"
-echo "P_YFII=$P_YFII" >> $output_file
-
-P_QUSD=`npx oz create -n $NETWORK PERC20 --init initialize --args $QUSD,$COMPTROLLER,$JUMP_RATE_MODEL,0.2e27,\"WePiggy\ QUSD\",\"pQUSD\",8`
-print_progress "P_QUSD = $P_QUSD"
-echo "P_QUSD=$P_QUSD" >> $output_file
-
-P_HUSD=`npx oz create -n $NETWORK PERC20 --init initialize --args $HUSD,$COMPTROLLER,$JUMP_RATE_MODEL,0.2e27,\"WePiggy\ HUSD\",\"pHUSD\",8`
-print_progress "P_HUSD = $P_HUSD"
-echo "P_HUSD=$P_HUSD" >> $output_file
-
-P_UNI=`npx oz create -n $NETWORK PERC20 --init initialize --args $UNI,$COMPTROLLER,$JUMP_RATE_MODEL,0.2e27,\"WePiggy\ UNI\",\"pUNI\",8`
-print_progress "P_UNI = $P_UNI"
-echo "P_UNI=$P_UNI" >> $output_file
+npx oz send-tx --to $WP_PRICE_PROVIDER_V1 --method addTokenConfig --args $P_ETH,$ETH,ETH,1e18,false,\[$ETH_CHAINLINK\],\[0\]
+npx oz send-tx --to $WP_PRICE_PROVIDER_V1 --method addTokenConfig --args $P_WBTC,$WBTC,WBTC,1e8,false,\[$WBTC_CHAINLINK\],\[0\]
+npx oz send-tx --to $WP_PRICE_PROVIDER_V1 --method addTokenConfig --args $P_DAI,$DAI,DAI,1e18,false,\[$DAI_CHAINLINK\],\[0\]
+npx oz send-tx --to $WP_PRICE_PROVIDER_V1 --method addTokenConfig --args $P_USDT,$USDT,USDT,1e6,true,\[$ZERO_ADDR\],\[0\]
+npx oz send-tx --to $WP_PRICE_PROVIDER_V1 --method addTokenConfig --args $P_USDC,$USDC,USDC,1e6,true,\[$ZERO_ADDR\],\[0\]
 
 # config pToken
 npx oz send-tx --to $P_ETH --method _setReserveFactor --args 0.1e18
@@ -178,32 +145,20 @@ npx oz send-tx --to $P_DAI --method _setReserveFactor --args 0.05e18
 npx oz send-tx --to $P_USDT --method _setReserveFactor --args 0.2e18
 npx oz send-tx --to $P_USDC --method _setReserveFactor --args 0.1e18
 npx oz send-tx --to $P_WBTC --method _setReserveFactor --args 0.1e18
-npx oz send-tx --to $P_QUSD --method _setReserveFactor --args 0.1e18
-npx oz send-tx --to $P_HUSD --method _setReserveFactor --args 0.1e18
-npx oz send-tx --to $P_YFII --method _setReserveFactor --args 0.1e18
-npx oz send-tx --to $P_UNI --method _setReserveFactor --args 0.5e18
 
 #==============================config Comptroller==============================#
 npx oz send-tx --to $COMPTROLLER --method _setMaxAssets --args 20
-npx oz send-tx --to $COMPTROLLER --method _setPriceOracle --args $SIMPLE_PRICE_ORAClE
+npx oz send-tx --to $COMPTROLLER --method _setPriceOracle --args $WP_PRICE_PROVIDER_V1
 npx oz send-tx --to $COMPTROLLER --method _supportMarket --args $P_ETH
 npx oz send-tx --to $COMPTROLLER --method _supportMarket --args $P_DAI
 npx oz send-tx --to $COMPTROLLER --method _supportMarket --args $P_USDT
 npx oz send-tx --to $COMPTROLLER --method _supportMarket --args $P_USDC
 npx oz send-tx --to $COMPTROLLER --method _supportMarket --args $P_WBTC
-npx oz send-tx --to $COMPTROLLER --method _supportMarket --args $P_YFII
-npx oz send-tx --to $COMPTROLLER --method _supportMarket --args $P_QUSD
-npx oz send-tx --to $COMPTROLLER --method _supportMarket --args $P_HUSD
-npx oz send-tx --to $COMPTROLLER --method _supportMarket --args $P_UNI
 npx oz send-tx --to $COMPTROLLER --method _setCollateralFactor --args $P_ETH,0.75e18
 npx oz send-tx --to $COMPTROLLER --method _setCollateralFactor --args $P_DAI,0.75e18
 npx oz send-tx --to $COMPTROLLER --method _setCollateralFactor --args $P_USDT,0
 npx oz send-tx --to $COMPTROLLER --method _setCollateralFactor --args $P_USDC,0.75e18
 npx oz send-tx --to $COMPTROLLER --method _setCollateralFactor --args $P_WBTC,0.6e18
-npx oz send-tx --to $COMPTROLLER --method _setCollateralFactor --args $P_YFII,0.6e18
-npx oz send-tx --to $COMPTROLLER --method _setCollateralFactor --args $P_QUSD,0.6e18
-npx oz send-tx --to $COMPTROLLER --method _setCollateralFactor --args $P_HUSD,0.75e18
-npx oz send-tx --to $COMPTROLLER --method _setCollateralFactor --args $P_UNI,0.6e18
 npx oz send-tx --to $COMPTROLLER --method _setCloseFactor --args 0.5e18
 npx oz send-tx --to $COMPTROLLER --method _setDistributeWpcPaused --args true
 
@@ -218,24 +173,19 @@ print_progress "PIGGY_DISTRIBUTION = $PIGGY_DISTRIBUTION"
 echo "PIGGY_DISTRIBUTION=$PIGGY_DISTRIBUTION" >> $output_file
 
 npx oz send-tx --to $COMPTROLLER --method _setPiggyDistribution --args $PIGGY_DISTRIBUTION
-npx oz send-tx --to $PIGGY_DISTRIBUTION --method _addWpcMarkets --args [$P_ETH,$P_DAI,$P_USDT,$P_USDC,$P_WBTC,$P_YFII,$P_QUSD,$P_HUSD,$P_UNI]
-npx oz send-tx --to $PIGGY_BREEDER --method add --args 1000,$MOCK_TOKEN,0x0000000000000000000000000000000000000000,false
+npx oz send-tx --to $PIGGY_DISTRIBUTION --method _addWpcMarkets --args [$P_ETH,$P_DAI,$P_USDT,$P_USDC,$P_WBTC]
+npx oz send-tx --to $PIGGY_BREEDER --method add --args 1000,$MOCK_TOKEN,$ZERO_ADDR,false
 # 给 PiggyDistribution转入对应的代币
 npx oz send-tx --to $MOCK_TOKEN --method transfer --args $PIGGY_DISTRIBUTION,1e20
 # 挖矿
 npx oz send-tx --to $PIGGY_DISTRIBUTION --method _stakeTokenToPiggyBreeder --args $MOCK_TOKEN,0
 
 # piggyBreeder,add pool
-npx oz send-tx --to $PIGGY_BREEDER --method add --args 1000,$P_ETH,0x0000000000000000000000000000000000000000,false
-npx oz send-tx --to $PIGGY_BREEDER --method add --args 1000,$P_DAI,0x0000000000000000000000000000000000000000,false
-npx oz send-tx --to $PIGGY_BREEDER --method add --args 1000,$P_USDT,0x0000000000000000000000000000000000000000,false
-npx oz send-tx --to $PIGGY_BREEDER --method add --args 1000,$P_USDC,0x0000000000000000000000000000000000000000,false
-npx oz send-tx --to $PIGGY_BREEDER --method add --args 1000,$P_WBTC,0x0000000000000000000000000000000000000000,false
-npx oz send-tx --to $PIGGY_BREEDER --method add --args 1000,$P_USDC,0x0000000000000000000000000000000000000000,false
-npx oz send-tx --to $PIGGY_BREEDER --method add --args 1000,$P_YFII,0x0000000000000000000000000000000000000000,false
-npx oz send-tx --to $PIGGY_BREEDER --method add --args 1000,$P_QUSD,0x0000000000000000000000000000000000000000,false
-npx oz send-tx --to $PIGGY_BREEDER --method add --args 1000,$P_HUSD,0x0000000000000000000000000000000000000000,false
-npx oz send-tx --to $PIGGY_BREEDER --method add --args 1000,$P_UNI,0x0000000000000000000000000000000000000000,false
+npx oz send-tx --to $PIGGY_BREEDER --method add --args 1000,$P_ETH,$ZERO_ADDR,false
+npx oz send-tx --to $PIGGY_BREEDER --method add --args 1000,$P_DAI,$ZERO_ADDR,false
+npx oz send-tx --to $PIGGY_BREEDER --method add --args 1000,$P_USDT,$ZERO_ADDR,false
+npx oz send-tx --to $PIGGY_BREEDER --method add --args 1000,$P_WBTC,$ZERO_ADDR,false
+npx oz send-tx --to $PIGGY_BREEDER --method add --args 1000,$P_USDC,$ZERO_ADDR,false
 
 END=`date +%s`
 
