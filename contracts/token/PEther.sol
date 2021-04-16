27082f8a8f5da256ff1b8b25e93ed732355afd6d
wepiggy-dev <coredevs@wepiggy.com>
2021-04-16 09:54:15 +0800

    Revert "flashloan"
    This reverts commit 04daaef5253d5dd91e4ddf2482bf022ff088cbe7.

--- contracts/token/PEther.sol
+++ contracts/token/PEther.sol
@@ -1,13 +1,27 @@
+// https://github.com/compound-finance/compound-protocol/blob/master/contracts/CEther.sol
+//Copyright 2020 Compound Labs, Inc.
+//Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+//1. Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+//2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
+//3. Neither the name of the copyright holder nor the names of its contributors may be used to endorse or promote products derived from this software without specific prior written permission.
+//THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
 // SPDX-License-Identifier: MIT
-pragma solidity 0.6.12;
+pragma solidity ^0.6.0;
 
 import "./PToken.sol";
-import "../flashloan/IFlashloan.sol";
+import "../comptroller/IComptroller.sol";
 
 contract PEther is PToken {
-
-    IFlashloan public flashloanInstance;
-
+    /**
+     * @notice Construct a new PEther money market
+     * @param comptroller_ The address of the Comptroller
+     * @param interestRateModel_ The address of the interest rate model
+     * @param initialExchangeRateMantissa_ The initial exchange rate, scaled by 1e18
+     * @param name_ ERC-20 name of this token
+     * @param symbol_ ERC-20 symbol of this token
+     * @param decimals_ ERC-20 decimal precision of this token
+     */
     function initialize(IComptroller comptroller_,
         IInterestRateModel interestRateModel_,
         uint initialExchangeRateMantissa_,
@@ -17,7 +31,12 @@ contract PEther is PToken {
         super.__Ownable_init();
         super.init(comptroller_, interestRateModel_, initialExchangeRateMantissa_, name_, symbol_, decimals_);
     }
+    /*** User Interface ***/
 
+    /**
+     * @notice Sender supplies assets into the market and receives cTokens in exchange
+     * @dev Reverts upon any failure
+     */
     function mint() external payable {
         (uint err,) = mintInternal(msg.value);
         requireNoError(err, "mint failed");
@@ -28,49 +47,102 @@ contract PEther is PToken {
         requireNoError(err, "mint failed");
     }
 
+    /**
+     * @notice Sender redeems cTokens in exchange for the underlying asset
+     * @dev Accrues interest whether or not the operation succeeds, unless reverted
+     * @param redeemTokens The number of cTokens to redeem into underlying
+     * @return uint 0=success, otherwise a failure (see ErrorReporter.sol for details)
+     */
     function redeem(uint redeemTokens) external returns (uint) {
         return redeemInternal(redeemTokens);
     }
 
+    /**
+     * @notice Sender redeems cTokens in exchange for a specified amount of underlying asset
+     * @dev Accrues interest whether or not the operation succeeds, unless reverted
+     * @param redeemAmount The amount of underlying to redeem
+     * @return uint 0=success, otherwise a failure (see ErrorReporter.sol for details)
+     */
     function redeemUnderlying(uint redeemAmount) external returns (uint) {
         return redeemUnderlyingInternal(redeemAmount);
     }
 
+    /**
+      * @notice Sender borrows assets from the protocol to their own address
+      * @param borrowAmount The amount of the underlying asset to borrow
+      * @return uint 0=success, otherwise a failure (see ErrorReporter.sol for details)
+      */
     function borrow(uint borrowAmount) external returns (uint) {
         return borrowInternal(borrowAmount);
     }
 
+    /**
+     * @notice Sender repays their own borrow
+     * @dev Reverts upon any failure
+     */
     function repayBorrow() external payable {
         (uint err,) = repayBorrowInternal(msg.value);
         requireNoError(err, "repayBorrow failed");
     }
 
+    /**
+     * @notice Sender repays a borrow belonging to borrower
+     * @dev Reverts upon any failure
+     * @param borrower the account with the debt being payed off
+     */
     function repayBorrowBehalf(address borrower) external payable {
         (uint err,) = repayBorrowBehalfInternal(borrower, msg.value);
         requireNoError(err, "repayBorrowBehalf failed");
     }
 
+    /**
+     * @notice The sender liquidates the borrowers collateral.
+     *  The collateral seized is transferred to the liquidator.
+     * @dev Reverts upon any failure
+     * @param borrower The borrower of this cToken to be liquidated
+     * @param pTokenCollateral The market in which to seize collateral from the borrower
+     */
     function liquidateBorrow(address borrower, PToken pTokenCollateral) external payable {
         (uint err,) = liquidateBorrowInternal(borrower, msg.value, pTokenCollateral);
         requireNoError(err, "liquidateBorrow failed");
     }
 
+    /**
+     * @notice Send Ether to CEther to mint
+     */
     receive() external payable {
+        (uint err,) = mintInternal(msg.value);
+        requireNoError(err, "mint failed");
     }
 
+    /*** Safe Token ***/
+
+    /**
+     * @notice Gets balance of this contract in terms of Ether, before this message
+     * @dev This excludes the value of the current message, if any
+     * @return The quantity of Ether owned by this contract
+     */
     function getCashPrior() internal override view returns (uint) {
         (MathError err, uint startingBalance) = subUInt(address(this).balance, msg.value);
         require(err == MathError.NO_ERROR);
         return startingBalance;
     }
 
+    /**
+     * @notice Perform the actual transfer in, which is a no-op
+     * @param from Address sending the Ether
+     * @param amount Amount of Ether being sent
+     * @return The actual amount of Ether transferred
+     */
     function doTransferIn(address from, uint amount) internal override returns (uint) {
+        // Sanity checks
         require(msg.sender == from, "sender mismatch");
         require(msg.value == amount, "value mismatch");
         return amount;
     }
 
     function doTransferOut(address payable to, uint amount) internal override {
+        /* Send the Ether, with minimal gas and revert on failure */
         to.transfer(amount);
     }
 
@@ -78,29 +150,21 @@ contract PEther is PToken {
         if (errCode == uint(Error.NO_ERROR)) {
             return;
         }
+
         bytes memory fullMessage = new bytes(bytes(message).length + 5);
         uint i;
+
         for (i = 0; i < bytes(message).length; i++) {
             fullMessage[i] = bytes(message)[i];
         }
+
         fullMessage[i + 0] = byte(uint8(32));
         fullMessage[i + 1] = byte(uint8(40));
         fullMessage[i + 2] = byte(uint8(48 + (errCode / 10)));
         fullMessage[i + 3] = byte(uint8(48 + (errCode % 10)));
         fullMessage[i + 4] = byte(uint8(41));
+
         require(errCode == uint(Error.NO_ERROR), string(fullMessage));
-    }
-
-    function flashloan(address _receiver, uint256 _amount, bytes memory _params) nonReentrant external {
-        uint256 cashBefore = getCashPrior();
-        doTransferOut(address(uint160(address(flashloanInstance))), _amount);
-        flashloanInstance.flashloan(address(this), _receiver, address(0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE), _amount, _params);
-        require(getCashPrior() >= cashBefore, "The actual balance is inconsistent");
-        accrueInterest();
-    }
-
-    function _setFlashloan(address _flashloan) public onlyOwner {
-        flashloanInstance = IFlashloan(_flashloan);
     }
 
 }
